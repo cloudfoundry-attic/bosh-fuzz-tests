2939ecf508cbf72fe78e3f3a46f8b1c1119293a6
Andrew Su <asu@pivotal.io>
2017-04-13 11:02:50 -0400

    WIP: Removed extra line of code.
    [#143268349](https://www.pivotaltracker.com/story/show/143268349)
    
    Signed-off-by: Jamil Shamy <jshamy@pivotal.io>

--- src/github.com/cloudfoundry-incubator/bosh-fuzz-tests/deployment/deployer.go
+++ src/github.com/cloudfoundry-incubator/bosh-fuzz-tests/deployment/deployer.go
@@ -85,7 +85,6 @@ func (d *deployer) RunDeploys() error {
 			}
 
 			deployWrapper := bltaction.NewDeployWrapper(d.cliRunner)
-			taskId, err := deployWrapper.RunWithDebug("deploy", manifestPath.Name())
 			taskId, err := deployWrapper.RunWithDebug("-d", "foo-deployment", "deploy", manifestPath.Name())
 			if err != nil {
 				errorPrefix := ""
